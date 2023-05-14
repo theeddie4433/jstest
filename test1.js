79834ab7572b7188c9374e1880b7213b6a2ac09d
eddieor <eortizr06@gmail.com>
2023-05-13 19:48:56 -0700

    ejercicio de busqueda usando for of  y ejercicio de funcion filter en array(test1)

---
+++ test1.js
@@ -0,0 +1,9 @@
+function lCat(cat) {
+  return cat.startsWith("L");
+}
+
+const cats = ["Leopard", "Serval", "Jaguar", "Tiger", "Caracal", "Lion"];
+
+const filtered = cats.filter(lCat);
+
+console.log(filtered);
