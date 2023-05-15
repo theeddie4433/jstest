a64db4bb4a1caf0983fd2bb78728055394e8ec5e
eddieor <eortizr06@gmail.com>
2023-05-15 16:47:10 -0700

    countdown loop function added, bug fixed (i variable wasnt comparing in conditional) working

--- countdown.js
+++ countdown.js
@@ -3,14 +3,19 @@ output.innerHTML = '';
 
 
 for (let i = 10; i>=0; i--) {
+   
     const para = document.createElement('p');
-    if(i=10){
-    para.textContent = "Countdown:" + i ;
-    } else if(i=0){
+
+    if(i===10){
+    para.textContent = "Countdown: " + i ;
+    console.log(para.textContent);
+    } else if(i===0){
         para.textContent = "0";
         para.textContent = "Launch!!";
+        console.log(para.textContent);
     }else {
         para.textContent = i;
+        console.log(para.textContent);
     }
     output.appendChild(para);
 }
