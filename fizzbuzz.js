97c38ca4139ded38efca35472ab2249ff8db22c9
eddieor <eortizr06@gmail.com>
2023-05-28 17:21:55 -0700

    js modified added queryselector and replaced console log for innerHTML for output

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -6,16 +6,17 @@ If the current number is divisible by 3 and 5 then print "FizzBuzz"
 Otherwise print the current number*/
 
 let number = parseInt(prompt("Escribe el numero que quieres fizzbuzz: "));
+const para = document.querySelector("div");
 
 for (let i = 1; i <= number; i++) {
 
 if (i % 3 === 0 && i % 5 === 0) {
-        console.log("FizzBuzz");
+        para.innerHTML += "FizzBuzz<br>";
 }else if(i % 3 === 0){
-    console.log("Fizz");
+    para.innerHTML += "\nFizz<br>";
 }else if (i % 5 === 0){
-    console.log("Buzz");
+    para.innerHTML += "\nBuzz<br>";
 }else {
-    console.log(i);
+    para.innerHTML += i + "<br>" ;
 }
 }        
